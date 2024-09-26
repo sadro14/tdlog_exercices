8935a73e85fffe05e0f66d8fe898f32a727d6e19
sadro14 <148468180+sadro14@users.noreply.github.com>
2024-09-26 16:33:18 +0100

    Exo2/Sadrac-YAMEOGO

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -28,3 +28,52 @@ fixed_tests_False = (
     ( "spam",    "eggs"  )
 )
 """
+
+
+
+
+class Strings:
+    
+    def __init__(self, list_of_string):
+        self.list_of_string=list_of_string
+        self.True_or_False=self.check_strings(list_of_string)
+
+    def check_strings(self, listofstrings):
+        for string in listofstrings :
+            first_arg, second_arg = string[0], string[1]
+            first, second = len(first_arg), len(second_arg)
+            if first>=second :
+                result=first_arg[first-second:]
+                if result!= second_arg:
+                   return False
+                    
+            else :
+                return False
+        return True
+    
+ 
+
+
+if __name__ == '__main__':
+    fixed_tests_False = [
+            ("sumo", "omo"),
+            ("samurai", "ra"),
+            ("abc", "abcd"),
+            ("ails", "fails"),
+            ("this", "fails"),
+            ("spam", "eggs")
+        ]
+    fixed_tests_True = [
+            ("samurai", "ai"),
+            ("ninja", "ja"),
+            ("sensei", "i"),
+            ("abc", "abc"),
+            ("abcabc", "bc"),
+            ("fails", "ails")
+        ]
+
+    fixed_tests_True=Strings(fixed_tests_True)
+    print(f"fixed_tests_True is : {fixed_tests_True.True_or_False}")
+
+    fixed_tests_False=Strings(fixed_tests_False)
+    print(f"fixed_tests_False is : {fixed_tests_False.True_or_False}")
